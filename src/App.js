d51e26003750b17ff04af0af177da3200b95a46d
jam-jam200 <jessieoyaks4j@gmail.com>
2022-10-20 17:01:34 +0100

    starting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Advice Generator</title>
+        <link rel="canonical" href="http://darkmodefirstpush.netlify.app" />
+        <meta name="description" content="random advice generator" />
+      </Helmet>
+      hi
     </div>
   );
 }
