d584d21fda979d92d66215528df58bbe18d6359f
jam-jam200 <jessieoyaks4j@gmail.com>
2022-10-20 18:30:44 +0100

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,43 @@ import axios from "axios";
 
 function App() {
   const [text, setText] = useState([]);
-  // const fetchAdvice = async () => {
-  //   const res = await fetch("https://api.adviceslip.com/advice");
-  //   const data = await res.json();
-  //   console.log(data)
-  //   setText(data);
-  // };
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     axios
       .get("https://api.adviceslip.com/advice")
       .then((res) => {
-        console.log(res)
-        setText(res.data)
+        console.log(res);
+        setText(res.data.slip);
+        setLoading(false);
       })
       .catch((err) => {
-        console.log(err);
-      });
+        setError(err);
+      })
+      .finally(setLoading(true));
   }, []);
+
+  if (loading) {
+    return <p className="noData">Just a sec, your link is loading...</p>;
+  }
+  if (error) {
+    return <p className="noData">Sorry, something went wrong😓</p>;
+  }
+
+  const handleClick = () => {
+   axios
+     .get("https://api.adviceslip.com/advice")
+     .then((res) => {
+       console.log(res);
+       setText(res.data.slip);
+       setLoading(false);
+     })
+     .catch((err) => {
+       setError(err);
+     })
+     .finally(setLoading(true));
+  };
 
   return (
     <div className="container">
@@ -34,18 +53,14 @@ function App() {
         <link rel="canonical" href="http://darkmodefirstpush.netlify.app" />
         <meta name="description" content="random advice generator" />
       </Helmet>
-      {
-        
-      }
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, odit?
-      </p>
+      <h1>Advice #{text.id}</h1>
+      <p>{text.advice}</p>
       <picture>
         <source media="(min-width: 768px)" srcSet={pauseDesktop} />
         <img src={pauseMobile} alt="" />
       </picture>
       <div>
-        <button>
+        <button onClick={handleClick}>
           <img src={dice} alt="" />
         </button>
       </div>
